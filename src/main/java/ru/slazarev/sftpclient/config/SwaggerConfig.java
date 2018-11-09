c37c0815c0e9aed1fb02de8c3959beb228115d59
slazarev <slazarev@navicons.ru>
2018-11-09 14:43:05 +0300

    added API swagger-ui for Rest-controllers

--- src/main/java/ru/slazarev/sftpclient/config/SwaggerConfig.java
+++ src/main/java/ru/slazarev/sftpclient/config/SwaggerConfig.java
@@ -1,4 +1,23 @@
 package ru.slazarev.sftpclient.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.slazarev.sftpclient.SFTPClientApplication;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(SFTPClientApplication.class.getPackage().getName()))
+                .build();
+    }
 }
